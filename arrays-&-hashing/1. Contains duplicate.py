d046bcbad98ab35fcd093e717f8033ba8457d560
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-04 12:52:22 -0300

    :sparkles: second solution for contains duplicate

--- arrays-&-hashing/1. Contains duplicate.py
+++ arrays-&-hashing/1. Contains duplicate.py
@@ -6,11 +6,11 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # Criar set remove duplicatas
+        # O set remove números duplicados
         unique_nums = set(nums)
-        print(f"Unique nums: {unique_nums}")
+        # print(f"Unique nums: {unique_nums}")
 
-        # Se tamanhos diferentes, havia duplicatas
+        # Se os arrays forem de tamanhos diferentes, há números duplicados
         if len(nums) != len(unique_nums):
             return True
         else:
@@ -22,5 +22,25 @@ print(f"Result: {result}")
 
 """
     Unique nums: {1, 2, 3}
+    Result: True
+"""
+
+# 🥷🏾 SECOND SOLUTION
+    # Memory:  52.3 MB
+    # Runtime: 0.861 seconds
+
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # print(f"Unique nums: {set(nums)}")
+        return len(nums) != len(set(nums))
+    
+solution = Solution()
+result = solution.containsDuplicate([4, 4, 5, 6])
+print(f"Result: {result}")
+
+"""
+    Unique nums: {4, 5, 6}
     Result: True
 """
