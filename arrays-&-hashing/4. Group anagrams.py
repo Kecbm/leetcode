a5af08a96a1f38f5f30c020f8f81ebffc8256d7a
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-10 17:57:40 -0300

    :sparkles: second solution for group anagrams

--- arrays-&-hashing/4. Group anagrams.py
+++ arrays-&-hashing/4. Group anagrams.py
@@ -46,3 +46,44 @@ result = solution.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
     create group: [abt]
     Result: [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
 """
+
+
+# 🥷🏾 SECOND SOLUTION
+    # Memory: 52.1 MB
+    # Runtime: 0.888 seconds
+class Solution2:
+    def groupAnagrams(self, strs):
+        from collections import defaultdict
+
+        groups = defaultdict(list)
+
+        for word in strs:
+            # Ordenar as letras da palavra ("chave")
+            key = ''.join(sorted(word))
+            print(f"word: {word}, key: {key}")
+
+            groups[key].append(word)
+            print(f"groups[{key}]: {groups[key]}")
+
+        # Retornar apenas os valores (grupos) do dicionário
+        return list(groups.values())
+    
+solution = Solution2()
+result = solution.groupAnagrams(["act","pots","tops","cat","stop","hat"])
+print(f"Result: {result}")
+
+"""
+    word: act, key: act
+    groups[act]: ['act']
+    word: pots, key: opst
+    groups[opst]: ['pots']
+    word: tops, key: opst
+    groups[opst]: ['pots', 'tops']
+    word: cat, key: act
+    groups[act]: ['act', 'cat']
+    word: stop, key: opst
+    groups[opst]: ['pots', 'tops', 'stop']
+    word: hat, key: aht
+    groups[aht]: ['hat']
+    Result: [['act', 'cat'], ['pots', 'tops', 'stop'], ['hat']]
+"""
