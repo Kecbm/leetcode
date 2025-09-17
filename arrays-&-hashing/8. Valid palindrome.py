0265fce5fb6b82f94759eb2ac3c9743e5d6adbba
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-17 10:31:37 -0300

    :memo: comments

--- arrays-&-hashing/8. Valid palindrome.py
+++ arrays-&-hashing/8. Valid palindrome.py
@@ -6,11 +6,12 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         cleaned_string = s
         for letter in string.punctuation:
+            # Retirando pontuação, espaços e convertendo para letra minusculas
             cleaned_string = cleaned_string.replace(letter, '').replace(' ', '').lower()
 
+        # Invertendo a string
         reversed_string = cleaned_string[::-1]
 
-        
         # print(f"Cleaned: {cleaned_string}")
         # print(f"Reversed: {reversed_string}")
 
