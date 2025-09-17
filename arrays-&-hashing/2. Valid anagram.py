9ea067c247534df53c5330ecdaa45b5888f94b0f
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-17 20:59:14 -0300

    :sparkles: valid anagram

---
+++ arrays-&-hashing/2. Valid anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        first = sorted(s)
+        print(f"First: {first}")
+        second = sorted(t)
+        print(f"Second: {second}")
+
+        return first == second
+    
+solution = Solution()
+result = solution.isAnagram("racecar", "carrace")
+print(f"Result: {result}")
+
+"""
+First: ['a', 'a', 'c', 'c', 'e', 'r', 'r']
+Second: ['a', 'a', 'c', 'c', 'e', 'r', 'r']
+Result: True
+"""
