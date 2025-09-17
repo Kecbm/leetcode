a7d8586b75fa6381ccb3e4855703bd31c524ee1e
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-17 18:44:20 -0300

    :construction: 4 solution for the valid parentheses

--- arrays-&-hashing/10. Valid parentheses.py
+++ arrays-&-hashing/10. Valid parentheses.py
@@ -15,6 +15,7 @@ Result: True
 
 class Solution2:
     def isValid2(self, s: str) -> bool:
+        # Validações da sequência de caracteres padrão
         if s.startswith("("):
             expected = "[{}])"
 
@@ -57,6 +58,7 @@ Result: False
 """
 
 class Solution3:
+    # Refatoração da lógica de validação
     def validated(self, s: str, expected: str) -> bool:
         for i in range(1, len(expected)):
             if s[i] != expected[i - 1]:
@@ -68,13 +70,14 @@ class Solution3:
         if len(s) < 2:
             return False
 
+        # Validações da sequência de 2 caracteres
         if len(s) == 2:
             if s.startswith("("):
-                return s.endswith(")")
+                return self.validated(s, ")")
             if s.startswith("["):
-                return s.endswith("]")
+                return self.validated(s, "]")
             if s.startswith("{"):
-                return s.endswith("}")
+                return self.validated(s, "}")
 
         if s.startswith("("):
             return self.validated(s, "[{}])")
@@ -85,8 +88,54 @@ class Solution3:
         if s.startswith("{"):
             return self.validated(s, "}")
         
+        return False
+        
 solution3 = Solution3()
 result = solution3.isValid3("[]")
+# print(f"Result: {result}")
+"""
+Result: True
+"""
+
+class Solution4:
+    def validated(self, s: str, expected: str) -> bool:
+        for i in range(1, len(expected)):
+            if s[i] != expected[i - 1]:
+                result = False
+                return result
+        
+        result = True
+        return result
+    
+    def isValid4(self, s: str) -> bool:
+        if len(s) < 2:
+            result = False
+
+        if len(s) == 2:
+            if s.startswith("("):
+                result = self.validated(s, ")")
+            if s.startswith("["):
+                result = self.validated(s, "]")
+            if s.startswith("{"):
+                result = self.validated(s, "}")
+
+        if s.startswith("("):
+            result = self.validated(s, "[{}])")
+        
+        if s.startswith("["):
+            result = self.validated(s, "{}]")
+
+        # Validações do novo tipo de sequência de caracteres
+        if s.startswith("("):
+            result = self.validated(s, ")[]{}")
+        
+        if s.startswith("["):
+            result = self.validated(s, "]{}")
+
+        return result
+
+solution4 = Solution4()
+result = solution4.isValid4("()[]{}")
 print(f"Result: {result}")
 """
 Result: True
