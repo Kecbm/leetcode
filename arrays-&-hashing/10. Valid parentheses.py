ee9438ab77f5c7510b9fab833aca9f21eb73ed2b
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-17 18:09:25 -0300

    :construction: second solution for the valid parentheses

--- arrays-&-hashing/10. Valid parentheses.py
+++ arrays-&-hashing/10. Valid parentheses.py
@@ -8,3 +8,50 @@ class Solution:
 solution = Solution()
 result = solution.isValid1("[]")
 # print(f"Result: {result}")
+
+"""
+Result: True
+"""
+
+class Solution2:
+    def isValid2(self, s: str) -> bool:
+        if s.startswith("("):
+            expected = "[{}])"
+
+            for i in range(1, len(expected)):
+                if s[i] != expected[i - 1]:
+                    return False
+            
+            return True
+        
+        if s.startswith("["):
+            expected = "{}]"
+
+            for i in range(1, len(expected)):
+                if s[i] != expected[i - 1]:
+                    return False
+            
+            return True
+        
+        if s.startswith("{"):
+            expected = "}"
+
+            for i in range(1, len(expected)):
+                if s[i] != expected[i - 1]:
+                    return False
+            
+            return True
+
+solution2 = Solution2()
+result = solution2.isValid2("([{}])")
+# print(f"Result: {result}")
+"""
+Result: True
+"""
+
+solution2 = Solution2()
+result = solution2.isValid2("[(])")
+print(f"Result: {result}")
+"""
+Result: False
+"""
