654e59ebfc943a130ad8c46b11b3f0cd7f4b64b9
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-17 20:59:41 -0300

    :memo: comment print

--- arrays-&-hashing/2. Valid anagram.py
+++ arrays-&-hashing/2. Valid anagram.py
@@ -1,15 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         first = sorted(s)
-        print(f"First: {first}")
+        # print(f"First: {first}")
         second = sorted(t)
-        print(f"Second: {second}")
+        # print(f"Second: {second}")
 
         return first == second
     
 solution = Solution()
 result = solution.isAnagram("racecar", "carrace")
-print(f"Result: {result}")
+# print(f"Result: {result}")
 
 """
 First: ['a', 'a', 'c', 'c', 'e', 'r', 'r']
