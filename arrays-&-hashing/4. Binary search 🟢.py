552626caf851fb2cd7d19780694666557aadb7ed
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-18 10:29:53 -0300

    :memo: comments on binary search

--- arrays-&-hashing/4. Binary search 🟢.py
+++ arrays-&-hashing/4. Binary search 🟢.py
@@ -2,15 +2,18 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # Verifica se o target existe na lista
         if target in nums:
+            # Se existie, retorna o index do target
             index = nums.index(target)
             return index
         
+        # Se não existir, retorna -1
         return -1
 
 solution = Solution()
 result = solution.search([-1,0,2,4,6,8], 4)
-print(f"Result: {result}")
+# print(f"Result: {result}")
 
 """
 Result: 3
