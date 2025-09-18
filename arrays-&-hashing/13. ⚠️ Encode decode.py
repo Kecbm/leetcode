5b6b74c14083093585065ab46ea16bb9d027ae19
Klecianny Melo <klecianny.melo@quorum.us>
2025-09-18 19:39:00 -0300

    :construction: encode decode

---
+++ arrays-&-hashing/13. ⚠️ Encode decode.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        result = "#".join(strs)
+        
+        return result
+        
+    def decode(self, s: str) -> List[str]:
+        if not s:
+            return []
+            
+        result = s.split("#")
+
+        return result
+
+
+solution = Solution()
+result = solution.encode(["neet","code","love","you"])
+# print(f"Result: {result}")
+"""
+Result: neet#code#love#you
+"""
+
+solution = Solution()
+result = solution.decode("neet#code#love#you")
+print(f"Result: {result}")
+"""
+Result: ['neet', 'code', 'love', 'you']
+"""
