97a6dfd522f059ae9df590057efb64f8c65696ad
Klecianny Melo <klecianny.melo@quorum.us>
2025-10-15 10:26:18 -0300

    :sparkles: sum of two integers

---
+++ bit-manipulation/24. Sum of two integers 🟢.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return sum((a, b))
+
+solution = Solution()
+result = solution.getSum(1, 1)
+print(f"Result: {result}")
+
+"""
+Result: 2
+"""
