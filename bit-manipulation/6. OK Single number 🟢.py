80a02cb2542d4f237317c79868d377a425dfb503
Klecianny Melo <klecianny.melo@quorum.us>
2025-10-21 11:25:27 -0300

    :sparkles: hash set on the single number

--- bit-manipulation/6. OK Single number 🟢.py
+++ bit-manipulation/6. OK Single number 🟢.py
@@ -28,3 +28,35 @@ Count: {7: 2, 6: 2, 8: 1}
 Single number: [8]
 Result: 8
 """
+
+
+# 🏆 Solution
+# Hash Set
+
+class Solution2:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen = set()
+        # print(f"🚀 Starting with nums: {nums}")
+        
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+                # print(f"❌ Removed {num} from seen. Current seen: {seen}")
+            else:
+                seen.add(num)
+                # print(f"✅ Added {num} to seen. Current seen: {seen}")
+        
+        result = list(seen)[0]
+        # print(f"🎯 Final result: {result}")
+        return result
+
+solution2 = Solution2()
+result = solution2.singleNumber([3,2,3])
+# print(result)
+"""
+🚀 Starting with nums: [3, 2, 3]
+✅ Added 3 to seen. Current seen: {3}
+✅ Added 2 to seen. Current seen: {2, 3}
+❌ Removed 3 from seen. Current seen: {2}
+🎯 Final result: 2
+"""
