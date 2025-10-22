3e9293471335943ce65b41290703e3c4ff5a62ce
Klecianny Melo <klecianny.melo@quorum.us>
2025-10-22 12:37:35 -0300

    :sparkles: missing number solution with hash set

--- bit-manipulation/7. OK Missing number 🟢.py
+++ bit-manipulation/7. OK Missing number 🟢.py
@@ -29,3 +29,30 @@ List ordered: [0, 1, 3]
 Missing number: 2
 Result: 2
 """
+
+
+# 🏆 Solution
+# Hash Set
+
+class Solution2:
+    def missingNumber(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        # print(f"🔢 Created set: {num_set}")
+        n = len(nums)
+        # print(f"📏 Array length: {n}")
+        for i in range(n + 1):
+            # print(f"🔍 Checking if {i} is in set: {i in num_set}")
+            if i not in num_set:
+                # print(f"🎯 Missing number found: {i}")
+                return i
+            
+solution2 = Solution2()
+result = solution2.missingNumber([0,2])
+
+"""
+🔢 Created set: {0, 2}
+📏 Array length: 2
+🔍 Checking if 0 is in set: True
+🔍 Checking if 1 is in set: False
+🎯 Missing number found: 1
+"""
