1e516fb1c29d0fe08beb40591c069c5c29632491
Klecianny Melo <klecianny.melo@quorum.us>
2025-12-02 16:43:45 -0300

    :sparkles: solution brute force for sum of two integers

--- bit-manipulation/24. Sum of two integers 🟡.py
+++ bit-manipulation/24. Sum of two integers 🟡.py
@@ -4,8 +4,23 @@ class Solution:
 
 solution = Solution()
 result = solution.getSum(1, 1)
-print(f"Result: {result}")
+# print(f"Result: {result}")
 
 """
 Result: 2
+"""
+
+# 🏆 Solution
+# Brute Force
+
+class Solution2:
+    def getSum2(self, a: int, b: int) -> int:
+        return a + b
+
+solution2 = Solution2()
+result = solution2.getSum2(4, 7)
+print(f"Result: {result}")
+
+"""
+Result: 11
 """
