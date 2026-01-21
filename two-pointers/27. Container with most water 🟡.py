2f14f1b4110c21a6e06b0ca53fcda8e863aadab9
Klecianny Melo <klecianny.melo@quorum.us>
2026-01-21 10:48:55 -0300

    :sparkles: second solution for cwmw

--- two-pointers/27. Container with most water 🟡.py
+++ two-pointers/27. Container with most water 🟡.py
@@ -11,3 +11,26 @@ result = solution.maxArea([2, 2, 2])
 """
 Result: 4
 """
+
+class Solution2:
+    def maxArea2(self, heights: List[int]) -> int:
+        # Two Pointers
+        # i is ths left
+        right = len(heights) - 1
+
+        for i in range(len(heights)):
+            if (heights[i] < heights[right]):
+                i += 1
+                continue
+            
+            if (heights[i] <= heights[right]):
+                return heights[i] * heights[right]
+
+    
+solution2 = Solution2()
+result2 = solution2.maxArea2([1,7,2,5,4,7,3,6])
+# print(f"Result: {result2}")
+
+"""
+Result: 36
+"""
